43245e23ffb6fa42bdac21c62c6c9293e312dc9b
okami <2717806614@qq.com>
2022-04-06 12:00:41 +0800

    测试

--- chapter 1/hello.c
+++ chapter 1/hello.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("hello");
-    //system("pause");
-    
-    return 0;
+int main() {
+  printf("hello");
+  // system("pause");
+
+  return 0;
 }
