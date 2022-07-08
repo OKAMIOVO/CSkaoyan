5d44f3f0ed774dc8761ec46113858ed884e98720
okami <2717806614@qq.com>
2022-07-08 23:22:33 +0800

    新考点

---
+++ new add/disjoint_set.c
@@ -0,0 +1,18 @@
+/**
+ * @file disjoint_set.c
+ * @author OKAMI (xiaomeimeiyao@163.com)
+ * @brief 并查集
+ * @version 0.1
+ * @date 2022-06-30 21:58:31
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+#define bool int
+#define true 1
+#define false 0
+
+#define SIZE 13
